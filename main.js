652d6afb0388faf8191732bf27f2b19daa0bd06f
rhjoung <rhjoung@gmail.com>
2026-04-09 06:41:22 +0000

    feat: allow image upload for classification

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
 const URL = "https://teachablemachine.withgoogle.com/models/J78gMY2UR/";
 
-let model, webcam, labelContainer, maxPredictions;
+let model, labelContainer, maxPredictions;
 
-async function init() {
+const uploadButton = document.getElementById('upload-button');
+const imageUpload = document.getElementById('image-upload');
+const imagePreview = document.getElementById('image-preview');
+
+async function loadModel() {
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
+    // load the model and metadata
     model = await tmImage.load(modelURL, metadataURL);
     maxPredictions = model.getTotalClasses();
 
-    const flip = true;
-    webcam = new tmImage.Webcam(200, 200, flip);
-    await webcam.setup();
-    await webcam.play();
-    window.requestAnimationFrame(loop);
-
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
     labelContainer = document.getElementById("label-container");
-    for (let i = 0; i < maxPredictions; i++) {
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
         labelContainer.appendChild(document.createElement("div"));
     }
 }
 
-async function loop() {
-    webcam.update();
-    await predict();
-    window.requestAnimationFrame(loop);
-}
-
-async function predict() {
-    const prediction = await model.predict(webcam.canvas);
+// run the webcam image through the image model
+async function predict(image) {
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(image);
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction =
             prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].innerHTML = classPrediction;
     }
 }
+
+uploadButton.addEventListener('click', () => imageUpload.click());
+
+imageUpload.addEventListener('change', async (event) => {
+    if (event.target.files && event.target.files[0]) {
+        const reader = new FileReader();
+        reader.onload = async (e) => {
+            imagePreview.src = e.target.result;
+            imagePreview.style.display = 'block';
+            if (!model) {
+                await loadModel();
+            }
+            await predict(imagePreview);
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+});
+
 
 const themeToggle = document.getElementById('checkbox');
 const body = document.body;
